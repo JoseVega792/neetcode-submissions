79cbe84368190c339c4bcd29a06177d26f32791f
Jose Vega <74834810+JoseVega792@users.noreply.github.com>
2026-06-24 21:45:04 -0500

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, n):
+        self.val = n
+        self.children = {}
+        self.end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node("")
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node(c)
+            curr = curr.children[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        queue = deque([self.root])
+        for c in word:
+            found = False
+            for node in range(len(queue)):
+                currNode = queue.popleft()
+                for child in currNode.children:
+                    if c == '.':
+                        found = True
+                        print(child, word)
+                        queue.append(currNode.children[child])
+                    else:
+                        if c == child:
+                            found = True
+                            print(c, word)
+                            queue.append(currNode.children[child])
+            if not found:
+                return False
+        for n in queue:
+            if n.end == True:
+                return True
+        return False
+        
+                    
