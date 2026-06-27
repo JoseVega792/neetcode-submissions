d42102b8186681ae8a7c31a256c0392f9729adbf
Jose Vega <74834810+JoseVega792@users.noreply.github.com>
2026-06-26 21:56:54 -0500

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def execute(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.execute(nums[1:]), self.execute(nums[:-1]))
