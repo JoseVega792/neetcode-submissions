7125a364f0b50fecde306ba49576b3f8476146a7
Jose Vega <74834810+JoseVega792@users.noreply.github.com>
2026-06-29 20:54:41 -0500

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        first, second = 0, 0
+        for i in range(len(nums)):
+            temp = max(first + nums[i], second)
+            first = second
+            second = temp
+        return second
